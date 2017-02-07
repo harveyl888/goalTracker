8418555ba8cfa6c34c6389c557f312326caf45bc
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-02-07 10:56:05 -0500

    create database

--- app.R
+++ app.R
@@ -7,6 +7,15 @@
 library(shiny)
 library(RSQLite)
 
+createDB <- function() {
+  db <- dbConnect(SQLite(), dbname='goals.sqlite')
+  dbSendQuery(db,'CREATE TABLE mainGoals (name TEXT)')
+  dbSendQuery(db,'CREATE TABLE subGoals (name TEXT, start TEXT, end TEXT, percentComplete INTEGER)')
+  dbDisconnect(db)
+}
+
+if (!file.exists('goals.sqlite')) createDB()
+
 server <- function(input, output) {
   
 }
