1db32ef2c7fd8f2b3fc29f9884fdae0ac28d3186
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-02-07 13:15:03 -0500

    add new main goal

--- app.R
+++ app.R
@@ -5,6 +5,7 @@
 ##
 
 library(shiny)
+library(shinyExtra)
 library(RSQLite)
 
 createDB <- function() {
@@ -14,13 +15,39 @@ createDB <- function() {
   dbDisconnect(db)
 }
 
+## If database does not exist, create it
 if (!file.exists('goals.sqlite')) createDB()
 
+## Connect to database and read in tables
+db <- dbConnect(SQLite(), dbname='goals.sqlite')
+df.main <- dbReadTable(db, 'mainGoals')
+df.sub <- dbReadTable(db, 'subGoals')
+
 server <- function(input, output) {
+  
+  ## main and subgoal data frames are reactive
+  goals <- reactiveValues(main = df.main, sub = df.sub)
+  
+  observeEvent(input$butAddMain, {
+    showModal(modalDialog(
+      title = "Add Main Goal",
+      div(style='display:inline-block; vertical-align:middle;', textInput('txtMainName', 'Name')),
+      div(style='display:inline-block; vertical-align:middle;', actionButton('butMainConfirm', 'Add', class = 'btn action-button btn-success'))
+    ))
+  })
+  
+  observeEvent(input$butMainConfirm, {
+    goals$main[nrow(goals$main) + 1, ] <- input$txtMainName
+    removeModal()
+  })
   
 }
 
 ui <- fluidPage(
+  
+  SXPanel('panMainGoals', heading = 'Main Goals', text_size = 'large', styleclass = 'success',
+          actionButton('butAddMain', 'Add', class = 'btn action-button btn-success')
+          )
   
 )
 
