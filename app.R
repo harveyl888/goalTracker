b539e909d21d31b8da41d741c89b9c7148ac5110
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-02-07 10:20:41 -0500

    skeleton

---
+++ app.R
@@ -0,0 +1,19 @@
+##
+## Goaltracker
+##
+## Shiny app to manage and track goals
+##
+
+library(shiny)
+library(RSQLite)
+
+server <- function(input, output) {
+  
+}
+
+ui <- fluidPage(
+  
+)
+
+shinyApp(ui = ui, server = server)
+
